36fb35d991267312d9e8c7e262835ae137daf127
Joaquim Antônio de Medeiros Silva <20221101110042@ifrn.local>
2025-05-22 10:46:35 -0300

    Aula01

---
+++ Aula01/src/MyFirstComponent.jsx
@@ -0,0 +1,17 @@
+function  MyFirstComponent () {
+
+    const name = 'Clark Kent and Bruce Wayne'
+
+    return (
+        <>
+        <div>
+            <h1>Superman e o Batman</h1>
+        </div>
+        <div>
+            <p>{name}</p>
+        </div>
+        </>
+    )
+}
+
+export default MyFirstComponent 
