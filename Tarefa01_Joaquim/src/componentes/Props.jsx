fa839afa7c36cb182666da34e92bc8dd7f937ac7
Joaquim Antônio de Medeiros Silva <20221101110042@ifrn.local>
2025-05-28 14:09:09 -0300

    Tarefa01

--- Tarefa01_Joaquim/src/componentes/Props.jsx
+++ Tarefa01_Joaquim/src/componentes/Props.jsx
@@ -2,45 +2,49 @@ import React, { useState } from "react";
 import Card from "./Card";
 import Detalhamento from "./Detalhamento";
 import "./Props.css";
-
+import Superman from "../assets/superman.jpg"
+import MM from "../assets/mm.jpg"
+import Arrow from "../assets/arrow.jpg"
 import Aquaman from "../assets/aquaman.jpg"
+import Flash from "../assets/flash.jpg"
+import Batman from "../assets/batman.jpg"
 
 
 export default function Props() {
   const cards = [
     {
       id: 1,
-      imageUrl: "../assets/superman.jpg",
+      imageUrl: Superman,
       title: "Superman",
       detalhe: "Superman ou Super-Homem é um super-herói de histórias em quadrinhos publicadas pela DC Comics."
     },
     {
       id: 2,
-      imageUrl: "../assets/mm.jpg",
+      imageUrl: MM,
       title: "Mulher Maravilha",
       detalhe: "Mulher-Maravilha é uma personagem fictícia de histórias em quadrinhos publicadas pela editora estadunidense DC Comics."
     },
     {
       id: 3,
-      imageUrl: "../assets/arrow.jpg",
+      imageUrl: Arrow,
       title: "Arqueiro Verde",
       detalhe: "Arqueiro Verde, alter-ego de Oliver Jonas Queen, é um personagem de história em quadrinhos do Universo DC"
     },
     {
       id: 4,
-      imageUrl: "../assets/aquaman.jpg",
+      imageUrl: Aquaman,
       title: "Aquaman",
       detalhe: "Aquaman é um filme de super-herói estadunidense de 2018, baseado no personagem homônimo da DC Comics"
     },
     {
       id: 5,
-      imageUrl: "../assets/flash.jpg",
+      imageUrl: Flash,
       title: "Flash",
       detalhe: "Flash é um nome compartilhado por diversos super-heróis da DC Comics."
     },
     {
       id: 6,
-      imageUrl: "../assets/batman.jpg",
+      imageUrl: Batman,
       title: "Batman",
       detalhe: "Batman é um personagem fictício e super-herói da editora norte-americana DC Comics."
     },
